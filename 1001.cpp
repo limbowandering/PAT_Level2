24444327166971f4a434d24924dc3b398b3a4ac9
limbowandering <591176276@qq.com>
2017-12-01 22:39:39 +0800

    -

--- 1001.cpp
+++ 1001.cpp
@@ -6,13 +6,20 @@ int main(){
   int n;
   cin >> n;
   int count = 0;
+  // while(n != 1){
+  //   if(n % 2 == 0){
+  //     n /= 2;
+  //   }else{
+  //     n = 3 * n + 1;
+  //     n /= 2;
+  //   }
+  //   count++;
+  // }
   while(n != 1){
-    if(n % 2 == 0){
-      n /= 2;
-    }else{
-      n = 3 * n + 1;
-      n /= 2;
+    if(n%2 != 0){
+      n = 3*n + 1;
     }
+    n /= 2;
     count++;
   }
   cout << count << endl;
