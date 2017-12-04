fbbcb98235137a9555a8a4807e46413f08d5f025
limbowandering <591176276@qq.com>
2017-12-04 12:20:20 +0800

    -

---
+++ 1003.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool PAT(string str) {
+    int len = str.length();
+    if (len <= 2)
+        return false;
+    bool book[3] = {false};
+    for (int i = 0; i < len; i++) {
+        if(str[i] == 'P')
+            book[0] = true;
+        else if(str[i] == 'A')
+            book[1] = true;
+        else if(str[i] == 'T')
+            book[2] = true;
+        else
+            return false;
+            if (str[i] == 'P') {
+            for (int j = 0; j < i; j++) {
+                if (str[j] != 'A')
+                    return false;
+            }
+            if (str [i+1] != 'A')
+                return false;
+            for (int k = i + 2; k < len; k++) {
+                if (str[k] != 'A') {
+                    if (str[k] != 'T') {
+                        return false;
+                    }
+                    if ((len - 1 - k) != ((k - 1 - i) * i))
+                        return false;
+                    for (int m = k + 1; m < len; m++) {
+                        if (str[m] != 'A')
+                            return false;
+                    }
+                }
+            }
+        }
+    }
+    if(book[0] == false || book[1] == false || book[2] == false)
+        return false;
+    return true;
+}
+
+int main(){
+  int n;
+  string m;
+  cin >> n;
+  while(n--){
+    cin >> m;
+    if(PAT(m)){
+      cout << "YES" << endl;
+    }
+    else{
+      cout << "NO" << endl;
+    }
+  }
+  return 0;
+}
